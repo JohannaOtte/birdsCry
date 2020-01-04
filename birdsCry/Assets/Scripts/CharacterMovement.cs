48f7ffd7889faa6834674fa87b6e3f7f39981650
Stefanwi <stewi98@googlemail.com>
2020-01-04 10:22:13 +0100

    Add basic movement

---
+++ birdsCry/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private bool m_RawInput = false;
+    [SerializeField] private float m_Speed = 12f;
+    [SerializeField] private float m_Gravity = -9.81f;
+
+
+    [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private float m_GroundDistance = 0.4f;
+    [SerializeField] private LayerMask m_GroundMask;
+
+    private CharacterController m_Controller = null;
+
+    public Vector3 m_Velocity;
+    public bool m_IsGrounded = false;
+
+    private void Start()
+    {
+        m_Controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        CheckIfGrounded();
+        Move();
+        StickToGround();
+       
+    }
+
+    private void CheckIfGrounded()
+    {
+        m_IsGrounded = Physics.CheckSphere(m_GroundCheck.position, m_GroundDistance, m_GroundMask);
+
+        if (m_IsGrounded)
+        {
+            m_Velocity.y = -2;
+        }
+    }
+
+    
+
+    private void Move()
+    {
+        float x = -1;
+        float z = -1;
+
+        if (m_RawInput)
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+        }
+
+        else
+        {
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+        }
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        m_Controller.Move(move * m_Speed * Time.deltaTime);
+    }
+
+    private void StickToGround()
+    {
+        if (!m_IsGrounded)
+        {
+            m_Velocity.y += m_Gravity + Time.deltaTime;
+        }
+        
+        m_Controller.Move(m_Velocity * Time.deltaTime);
+    }
+}
---
+++ birdsCry/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float m_MouseSensitivity = 100f;
+
+
+   [SerializeField] private Transform m_PlayerTransform = null;
+    private float m_XRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAround();
+    }
+
+    private void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.deltaTime;
+
+        m_XRotation -= mouseY;
+        m_XRotation = Mathf.Clamp(m_XRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(m_XRotation, 0f, 0f);
+
+        m_PlayerTransform.Rotate(Vector3.up * mouseX);
+
+    }
+}
