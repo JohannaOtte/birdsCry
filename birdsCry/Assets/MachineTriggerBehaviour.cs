63b59b95b4debeb60090c8f5a9e40a47426efc02
Stefanwi <stewi98@googlemail.com>
2020-01-04 11:13:21 +0100

    Add selection highlighting and TextMeshPro package

---
+++ birdsCry/Assets/MachineTriggerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineTriggerBehaviour : MonoBehaviour
+{
+    [SerializeField] private Color m_SelectedColor;
+
+    private Color m_NormalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_NormalColor = GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnSelected()
+    {
+        GetComponent<MeshRenderer>().material.color = m_SelectedColor;
+    }
+
+    private void OnDeselected()
+    {
+        GetComponent<MeshRenderer>().material.color = m_NormalColor;
+    }
+}
--- birdsCry/Assets/Scripts/MouseLook.cs
+++ birdsCry/Assets/Scripts/MouseLook.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] private float m_MouseSensitivity = 100f;
+   [SerializeField] private float m_MouseSensitivity = 100f;
 
 
    [SerializeField] private Transform m_PlayerTransform = null;
     private float m_XRotation = 0f;
+
+    private GameObject m_CurrentSelection = null;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,9 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         LookAround();
+        Raycast();
     }
+
 
     private void LookAround()
     {
@@ -34,5 +39,42 @@ public class MouseLook : MonoBehaviour
 
         m_PlayerTransform.Rotate(Vector3.up * mouseX);
 
+    }
+
+    private void Raycast()
+    {
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        GameObject selection = null;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            selection = hit.collider.transform.gameObject;
+            Debug.Log(selection);
+            if(selection == m_CurrentSelection)
+            {
+                return;
+            }
+
+
+
+            if(selection != null && selection.tag == "MachineTrigger" || selection.tag == "Bird")
+            {
+                selection.SendMessageUpwards("OnSelected");
+            }
+
+            if (m_CurrentSelection != null && (m_CurrentSelection.tag == "MachineTrigger" || m_CurrentSelection.tag == "Bird"))
+            {
+                m_CurrentSelection.SendMessageUpwards("OnDeselected");
+            }
+
+            m_CurrentSelection = selection;
+
+        }
+
+        
+
+       
     }
 }
